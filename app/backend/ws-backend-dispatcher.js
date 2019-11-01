13131c628cd6723635086559357749eed263560f
Simon Oulevay <simon.oulevay@gmail.com>
2019-11-01 15:39:46 +0100

    Implement backend & frontend with wamp

--- app/backend/ws-backend-dispatcher.js
+++ app/backend/ws-backend-dispatcher.js
@@ -1,7 +1,9 @@
+const autobahn = require('autobahn');
+const WebSocket = require('ws')
+
 const GameController = require('../backend/controller/game.controller')
 const PlayerController = require('../backend/controller/player.controller')
 const GameManager = require('../class/game-manager.class')
-const WebSocket = require('ws')
 
 const gameManager = new GameManager()
 const playerController = new PlayerController(gameManager)
@@ -11,6 +13,78 @@ const gameController = new GameController(gameManager)
 // ---- GAME MANAGEMENT
 
 // --- init websocket
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+const connection = new autobahn.Connection({
+  url: 'wss://wamp.archidep.media/ws',
+  realm: 'realm1'
+  /*authid: 'jdoe',
+  authmethods: [ 'ticket' ],
+  onchallenge: function() {
+    console.log('@@@ on challenge', JSON.stringify(Array.prototype.slice(arguments)));
+    return 'letmein';
+  }*/
+});
+
+connection.onopen = function(session) {
+  console.log('Connection to WAMP router established');
+
+  session.register('ch.comem.archioweb.tictactoe.getPlayer', function() {
+    return playerController.createPlayer();
+  });
+
+  session.register('ch.comem.archioweb.tictactoe.getJoinableGames', function() {
+    return gameController.getJoinableGames();
+  });
+
+  session.register('ch.comem.archioweb.tictactoe.createNewGame', function(args) {
+    const player = args[0];
+    const newGame = gameController.createNewGame(player);
+    session.publish('ch.comem.archioweb.tictactoe.newGames', [ newGame ]);
+    return newGame;
+  });
+
+  session.register('ch.comem.archioweb.tictactoe.requestJoinGame', function(args) {
+
+    const gameId = args[0];
+    const playerId = args[1];
+    const result = gameController.requestJoinGame(gameId, playerId);
+    if (result === 'invalidGame') {
+      throw new Error('Invalid game');
+    }
+
+    session.publish('ch.comem.archioweb.tictactoe.removedGames', [ result.game.id ]);
+
+    return result.game;
+  });
+
+  session.register('ch.comem.archioweb.tictactoe.exitGame', function(args) {
+
+    const gameId = args[0];
+    const playerId = args[1];
+    gameController.exitGame(gameId, playerId);
+
+    session.publish(`ch.comem.archioweb.tictactoe.games.${gameId}.exited`, [ playerId ]);
+    session.publish('ch.comem.archioweb.tictactoe.removedGames', [ gameId ]);
+  });
+
+  session.register('ch.comem.archioweb.tictactoe.updateBoard', function(args) {
+
+    const gameId = args[0];
+    const playerId = args[1];
+    const row = args[2];
+    const col = args[3];
+    const result = gameController.updateBoardRequest(gameId, playerId, row, col);
+    if (result === 'noGameFound') {
+      return;
+    } else if (result === 'invalidMove') {
+      throw [ 'Invalid move' ];
+    }
+
+    session.publish(`ch.comem.archioweb.tictactoe.games.${gameId}.moves`, [ row, col, result.playerIcon, result.hasWin, result.draw ]);
+  });
+};
+
+connection.open();
 
 // --- websocket route
 /*
--- app/frontend/ws-frontend-dispatcher.js
+++ app/frontend/ws-frontend-dispatcher.js
@@ -1,3 +1,5 @@
+const autobahn = require('autobahn');
+
 const LCS_MANAGER = require('./localstorage-manager')
 const ViewManager = require('./view-manager')
 
@@ -9,9 +11,137 @@ const ViewManager = require('./view-manager')
 const viewManager = new ViewManager()
 
 // ----------------------------------- GAME MANAGEMENT    
+function addNewJoinableGame(session, game) {
+  const player = LCS_MANAGER.load('player');
+  viewManager.addNewJoinableGame(player, game, (gameId, playerId) => {
+    session.call('ch.comem.archioweb.tictactoe.requestJoinGame', [ gameId, playerId ]).then(joinedGame => {
+      displayNewGame(session, joinedGame, player);
+    });
+  });
+}
+
+function createNewGame(session) {
+
+  const player = LCS_MANAGER.load('player');
+  if (!player) {
+    return viewManager.displayToast('No player defined');
+  }
+
+  session.call('ch.comem.archioweb.tictactoe.createNewGame', [ player ]).then(newGame => {
+    displayNewGame(session, newGame, player);
+  });
+}
+
+function displayNewGame(session, game, player) {
+  LCS_MANAGER.save('game', game);
+
+  let moveSubscription;
+  session.subscribe(`ch.comem.archioweb.tictactoe.games.${game.id}.moves`, function(args) {
+
+    const row = args[0];
+    const col = args[1];
+    const icon = args[2];
+    const win = args[3];
+    const draw = args[4];
+    updateBoard(col, row, icon);
+
+    if (win) {
+      viewManager.displayToast(`${icon} wins!`);
+    } else if (draw) {
+      viewManager.displayToast('Draw!');
+    }
+  }).then(sub => moveSubscription = sub);
+
+  let exitedSubscription;
+  session.subscribe(`ch.comem.archioweb.tictactoe.games.${game.id}.exited`, function(args) {
+
+    const playerId = args[0];
+    const player = LCS_MANAGER.load('player');
+    viewManager.displayToast(playerId === player.id ? 'You have left the game' : 'Your opponent has left the game');
+    viewManager.exitGame();
+
+    if (exitedSubscription) {
+      session.unsubscribe(exitedSubscription);
+    }
+
+    if (moveSubscription) {
+      session.unsubscribe(moveSubscription);
+    }
+  }).then(sub => exitedSubscription = sub);
+
+  viewManager.displayNewGame(player, game, (col, row) => {
+    session.call('ch.comem.archioweb.tictactoe.updateBoard', [ game.id, player.id, col, row ]).catch(err => {
+      if (err.args && err.args.length) {
+        viewManager.displayToast(err.args[0]);
+      } else {
+        console.warn(err);
+      }
+    });
+  });
+}
+
+function exitGame(session) {
+
+  const game = LCS_MANAGER.load('game');
+  const player = LCS_MANAGER.load('player');
+  if (!game || !player) {
+    return;
+  }
+
+  session.call('ch.comem.archioweb.tictactoe.exitGame', [ game.id, player.id ]);
+}
+
+function removeJoinableGame(gameId) {
+  viewManager.removeJoinableGame(gameId);
+}
+
+function updateBoard(col, row, icon) {
+  viewManager.updateBoard(row, col, icon);
+}
 
 
 // ----------------------------------- PLAYER MANAGEMENT
 
 
 // ----------------------------------- WEBSOCKET MANAGEMENT
+const connection = new autobahn.Connection({
+  url: 'wss://wamp.archidep.media/ws',
+  realm: 'realm1'
+  /*authid: 'jdoe',
+  authmethods: [ 'ticket' ],
+  onchallenge: function() {
+    console.log('@@@ on challenge', JSON.stringify(Array.prototype.slice(arguments)));
+    return 'letmein';
+  }*/
+});
+
+connection.onopen = function(session) {
+  console.log('Connection to WAMP router established');
+
+  session.call('ch.comem.archioweb.tictactoe.getPlayer', []).then(function(player) {
+    LCS_MANAGER.save('player', player);
+
+    viewManager.initEventManager(
+      () => createNewGame(session),
+      () => exitGame(session)
+    );
+
+    session.call('ch.comem.archioweb.tictactoe.getJoinableGames', []).then(function(games) {
+      for (const game of games) {
+        addNewJoinableGame(session, game);
+      }
+    });
+  });
+
+  session.subscribe('ch.comem.archioweb.tictactoe.newGames', function(args) {
+    const game = args[0];
+    addNewJoinableGame(session, game);
+  });
+
+  session.subscribe('ch.comem.archioweb.tictactoe.removedGames', function(args) {
+    const gameId = args[0];
+    removeJoinableGame(gameId);
+  });
+};
+
+connection.open();
